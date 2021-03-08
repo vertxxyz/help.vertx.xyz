9cd11c6004427933556eb34328a7c37a5a7002e8
Thomas Ingram <thomasingram@vertx.xyz>
2021-03-08 16:08:55 +1100

    Added sidebar and support for anchors.
    Closes #10

--- Troubleshooter/Assets/Content/Scripts/script.js
+++ Troubleshooter/Assets/Content/Scripts/script.js
@@ -2,18 +2,19 @@ const pageParameterKey = 'page';
 const main = 'Main';
 let currentDirectory = "";
 
-let pageParam = getParameterByKey(pageParameterKey);
+let pageParam = getPageParameter();
+console.log(window.location.href);
 console.log(`Initial Page Param: ${pageParam}`);
 //Don't push a history state for this change.
 if (pageParam === main) {
 	//If we've landed on the index then we should set our location as "Main".
-	setParameterByKey(pageParameterKey, '', false);
+	setParameterByKey(pageParameterKey, '', getHash(), false);
 	pageParam = null;
 } else {
-	setParameterByKey(pageParameterKey, pageParam, false);
+	setParameterByKey(pageParameterKey, pageParam, getHash(), false);
 }
 
-loadPageFromLink(pageParam, false);
+loadPageFromLink(pageParam, getHash(), false);
 
 
 /*************** CALLBACKS ****************/
@@ -24,14 +25,16 @@ window.onpopstate = loadPageFromState;
 function loadPageFromState(e) {
 	// page reload
 	if (e.state) {
-		console.log(`PopState: ${e.state.pathParameter}`);
-		loadPageFromLink(e.state.pathParameter, false, false);
+		console.log(`PopState: ${e.state.pathParameter} - ${e.state.hashParameter}`);
+		loadPageFromLink(e.state.pathParameter, e.state.hashParameter, false, false);
 	} else {
-
+		loadPageFromLink(getPageParameter(), getHash(), false, false);
 	}
 }
 
 /*************** FUNCTIONS *******************/
+
+function getPageParameter () { return getParameterByKey(pageParameterKey); }
 
 function getParameterByKey(key, url) {
 	if (!url) url = window.location.href;
@@ -43,15 +46,19 @@ function getParameterByKey(key, url) {
 	return results[2];
 }
 
-function setParameterByKey(key, value, pushHistory = true) {
-	const url = value == null || value === "" ? window.location.pathname : `?${key}=${value}`;
+function getHash () { return window.location.hash; }
 
-	console.log(`setParameterByName: ${url}`);
+function setParameterByKey(key, value, hash, pushHistory = true) {
+	let url = value == null || value === "" ? window.location.pathname : `?${key}=${value}`;
+
+	if(hash !== '')
+		url += hash;
+	console.log(`setParameterByName: ${url} - ${hash}`);
 	// State Object, Page Name, URL
 	if (pushHistory)
-		window.history.pushState({pathParameter: value}, document.title, url);
+		window.history.pushState({pathParameter: value, hashParameter: hash}, document.title, url);
 	else
-		window.history.replaceState({pathParameter: value}, document.title, url);
+		window.history.replaceState({pathParameter: value, hashParameter: hash}, document.title, url);
 }
 
 //Load Page is called from HTML
@@ -63,10 +70,10 @@ function loadPage(relativeLink) {
 	console.log(`Load Page: ${relativeLink}`);
 	if (relativeLink === main)
 		relativeLink = null;
-	loadPageFromLink(relativeLink);
+	loadPageFromLink(relativeLink, '');
 }
 
-function loadPageFromLink(value, setParameter = true, useCurrentDirectory = true) {
+function loadPageFromLink(value, hash, setParameter = true, useCurrentDirectory = true) {
 
 	if (value == null || value === "")
 		value = main;
@@ -84,15 +91,27 @@ function loadPageFromLink(value, setParameter = true, useCurrentDirectory = true
 
 	$(document).ready(function () {
 		const contents = $('.contents');
+		const sidebarContents = $('.sidebar-contents');
 		try {
 			contents.load(`HTML/${value}.html`, function (response, status, xhr) {
-			    if( status === "error") {
-			        load404();
-			        return;
-                }
+				if( status === "error") {
+					load404();
+					return;
+				}
 				if (setParameter)
-					setParameterByKey(pageParameterKey, value);
+					setParameterByKey(pageParameterKey, value, hash);
 				Prism.highlightAll();
+
+				if(hash !== ''){
+					/*contents.animate({
+				        scrollTop: $(hash).offset().top
+				    }, 400);*/
+					$(hash).get(0).scrollIntoView()
+				}
+			});
+			sidebarContents.load(`HTML/${value}_sidebar.html`, function (response, status, xhr) {
+				if( status === "error")
+					sidebarContents.empty();
 			});
 		} catch {
 			load404();
@@ -101,9 +120,9 @@ function loadPageFromLink(value, setParameter = true, useCurrentDirectory = true
 }
 
 function absolute(base, relative) {
-    const stack = base.split("/"),
-        parts = relative.split("/");
-    stack.pop(); // remove current file name (or empty string)
+	const stack = base.split("/"),
+		parts = relative.split("/");
+	stack.pop(); // remove current file name (or empty string)
 				 // (omit if "base" is the current folder without trailing slash)
 	for (let i = 0; i < parts.length; i++) {
 		if (parts[i] === ".")
@@ -118,5 +137,5 @@ function absolute(base, relative) {
 
 function load404() {
 	//console.log('loading 404')
-	loadPageFromLink(`404`, false, false);
+	loadPageFromLink(`404`, '', false, false);
 }
--- Troubleshooter/Assets/Site/Building/Common Build Errors.md
+++ Troubleshooter/Assets/Site/Building/Common Build Errors.md
@@ -4,3 +4,4 @@
 - [CS0246](Editor%20References.md) The type of namespace name '**UnityEditor**' could not be found, Are you missing an assembly reference?  
 - stdout: il2cpp.exe didn't catch exception: System.InvalidOperationException: [C++ code builder is unable to build C++ code.](Build%20Errors/IL2CPP%20Building.md)  
 - [Burst Compiler Failed Running](Build%20Errors/Burst%20Building.md)
+- ['' is an incorrect path for a scene file](Build%20Errors/Deleted%20Scenes.md). BuildPlayer expects path relative to the project folder.
